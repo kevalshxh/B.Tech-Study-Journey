abb0da4c48537dce1a8d25aadaa22819fdf54e4f
Keval Shah <sklshah21@gmail.com>
2026-07-23 19:18:49 +0530

    Add files via upload

---
+++ B.Tech/Semester - 1/C-Language/Summation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int a,b;
+    printf("Enter Value : ");
+    scanf("%d",&a);
+    printf("Enter Value : ");
+    scanf("%d",&b);
+    printf("Answer : %d",a+b);
+    return 0;
+}
---
+++ B.Tech/Semester - 1/C-Language/Value.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int value;
+    printf("Enter Value : ");
+    scanf("%d",&value);
+    printf("Value Entered Is : %d",value);
+    return 0;
+}
