28d51a2a82c3eeafb0f7421f4e648b2d677365a9
Keval Shah <299517791+kevalshxh@users.noreply.github.com>
2026-07-28 11:32:01 +0530

    Create EvenValue.c

---
+++ B.Tech/Semester - 1/C-Language/EvenValue.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int eve;
+    printf("Enter Value: ");
+    scanf("%d",&eve);
+    if(eve%2==0){
+        printf("Even Value");
+    }else{
+        printf("Odd Value");
+    }
+}
